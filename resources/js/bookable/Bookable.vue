1caade6b2ab1b800f31e6ed96483e3861acc5e5a
Mathieu Loisel <loiselmathieu35@gmail.com>
2020-10-20 12:29:20 +0200

    107. Price breakdown component

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -16,7 +16,18 @@
             <review-list :bookable-id="this.$route.params.id"></review-list>
         </div>
         <div class="col-md-4 pb-4">
-            <availability :bookable-id="this.$route.params.id" @availability="checkPrice($event)"></availability>
+            <availability 
+                :bookable-id="this.$route.params.id" 
+                @availability="checkPrice($event)" 
+                class="mb-4"
+            ></availability>
+            <transition name="fade">
+                <price-breakdown v-if="price" :price="price" class="mb-4"></price-breakdown>
+            </transition>
+            <transition name="fade">
+                <button class="btn btn-outline-secondary btn-block" v-if="price">Book now !</button>
+            </transition>
+                
         </div>
     </div>
 </template>
@@ -24,12 +35,15 @@
 <script>
 import Availability from './Availability'
 import ReviewList from './ReviewList'
+import PriceBreakdown from './PriceBreakdown'
+
 import {mapState} from 'vuex'
 
 export default {
     components: {
         Availability,
         ReviewList,
+        PriceBreakdown,
     },
     data(){
         return {
